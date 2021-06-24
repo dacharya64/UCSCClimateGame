19658e77d08a8a910db2f446922d761c07173efb
Devi Acharya <dacharya64@gmail.com>
2021-06-24 15:24:12 -0700

    colors now change depending on if value is going up or down

--- ClimatePrototypes/Assets/Scripts/City/Bill.cs
+++ ClimatePrototypes/Assets/Scripts/City/Bill.cs
@@ -30,13 +30,38 @@ public class Bill : MonoBehaviour {
 		List<RectTransform> showIcons = new List<RectTransform>();
 		foreach (Transform child in iconWrapper.transform) {
 			child.gameObject.SetActive(effects.ContainsKey(child.name));
+/*			if (effects.ContainsKey(child.name)) {
+				Debug.Log("Child name is " + child.name);
+				float result = effects[child.name];
+				Debug.Log("Result is: " + result);
+			}*/
+
+			//float result = effects[child.name];
+			//Debug.Log("Result is: " + result);
 			if (child.gameObject.activeSelf)
+            {
 				showIcons.Add(child as RectTransform);
+			}
 		}
 
 		float size = showIcons[0].rect.width;
 		int num = showIcons.Count;
 		foreach (var (child, i) in showIcons.Enumerator())
+		{
 			child.localPosition = new Vector2(size * ((i - num / 2) + (num % 2 == 1 ? 0 : .5f)), child.localPosition.y);
+			if (effects.ContainsKey(child.name))
+			{
+				Debug.Log("Child name is " + child.name);
+				float result = effects[child.name];
+				if (result > 0)
+				{
+					child.GetComponent<Image>().color = new Color32(0, 255, 0, 100);
+				}
+				else if (result < 0) {
+					child.GetComponent<Image>().color = new Color32(255, 0, 0, 100);
+				}
+				Debug.Log("Result is: " + result);
+			}
+		}
 	}
 }
