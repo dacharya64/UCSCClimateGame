b8a9454ca0044aa48e272ffbd7ecce861504b1a7
Devi Acharya <dacharya64@gmail.com>
2021-07-01 15:45:48 -0700

    implemented cancel button on options screen

--- ClimatePrototypes/Assets/Scripts/UI/OptionsMenu.cs
+++ ClimatePrototypes/Assets/Scripts/UI/OptionsMenu.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
-{
+public class OptionsMenu : MonoBehaviour { 
+    public float previousMusicVolume;
+    public float previousSFXVolume;
+    public Slider sfxSlider;
+    public Slider musicSlider;
+
    public void adjustMusicVolume(float volume) {
         AudioManager.Instance.UpdateMusicVolume(volume);
     }
@@ -15,6 +20,17 @@ public class OptionsMenu : MonoBehaviour
     public void resetToDefault() {
         AudioManager.Instance.UpdateMusicVolume(0.5f);
         AudioManager.Instance.UpdateSFXVolume(0.5f);
-        // Change toggle positions
+    }
+
+    public void savePreviousVolumes() {
+        previousMusicVolume = AudioManager.Instance.totalMusicVolume;
+        previousSFXVolume = AudioManager.Instance.totalSFXVolume;
+    }
+
+    public void resetToPreviousVolumes() {
+        musicSlider.value = previousMusicVolume;
+        sfxSlider.value = previousSFXVolume;
+        AudioManager.Instance.UpdateMusicVolume(previousMusicVolume);
+        AudioManager.Instance.UpdateSFXVolume(previousSFXVolume);
     }
 }
