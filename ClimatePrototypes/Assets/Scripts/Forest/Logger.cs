c0161281f58dfc8f3b231c2a64532bacbef55c4a
Devi Acharya <dacharya64@gmail.com>
2021-07-13 16:50:48 -0700

    now play tree chop SFX when logger cuts tree

--- ClimatePrototypes/Assets/Scripts/Forest/Logger.cs
+++ ClimatePrototypes/Assets/Scripts/Forest/Logger.cs
@@ -10,6 +10,7 @@ public class Logger : PathfindingAgent {
 
 public static class LoggerActions {
 	public static void Chop(Logger l) {
+		ForestController.Instance.StartCoroutine(PlayChopSFX());
 		l.anim.SetTrigger("Chopping");
 		l.transform.localScale = new Vector3(-1, 1, 1); // flip sprite
 		ForestController.Instance.StartCoroutine(ChopAndReturn(l));
@@ -19,5 +20,16 @@ public static class LoggerActions {
 		yield return ForestController.Instance.StartCoroutine(VolunteerActions.WaitAndReturn(l, 3));
 		ForestGrid.map.SetTile(l.choppingTile, ForestGrid.stump);
 		ForestGrid.RemoveTree(l.choppingTile);
+	}
+
+	public static IEnumerator PlayChopSFX() {
+		AudioManager.Instance.Play("SFX_Tree_Chop");
+		yield return new WaitForSeconds(0.8f);
+		AudioManager.Instance.Play("SFX_Tree_Chop");
+		yield return new WaitForSeconds(0.8f);
+		AudioManager.Instance.Play("SFX_Tree_Chop");
+		yield return new WaitForSeconds(0.8f);
+		AudioManager.Instance.Play("SFX_Tree_Chop");
+		yield return null;
 	}
 }
