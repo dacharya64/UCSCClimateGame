a3897c45e414c46e593741f73a2d915a009f56bc
Devi Acharya <dacharya64@gmail.com>
2021-07-15 15:47:24 -0500

    Adds slider that changes based on value of water

--- ClimatePrototypes/Assets/Scripts/Fire/SubtropicsPlayer.cs
+++ ClimatePrototypes/Assets/Scripts/Fire/SubtropicsPlayer.cs
@@ -10,6 +10,7 @@ public class SubtropicsPlayer : MonoBehaviour {
 	[SerializeField] Text leftWaterUI = default;
 	[SerializeField] Transform leftWaterBarUI = default;
 	[SerializeField] GameObject line = default;
+	[SerializeField] Slider waterTracker = default;
 	LineRenderer newLine;
 
 	Animator bladeAnimator;
@@ -40,6 +41,8 @@ public class SubtropicsPlayer : MonoBehaviour {
 	}
 
 	void Update() {
+		UpdateSlider(waterTracker, water);
+
 		lastUsedWater += Time.deltaTime; // TODO: do this timer logic better, maybe with coroutine
 
 		if (lastUsedWater >= 1f) // turn off renderer if not used water in 1 sec
@@ -91,17 +94,17 @@ public class SubtropicsPlayer : MonoBehaviour {
 				} else
 					neighbor.id = IdentityManager.Identity.Green;
 				neighbor.moisture = IdentityManager.Moisture.Moist;
-				water--; // use 1 water per cell
+				water = water - 5; // use 1 water per cell
 				lastUsedWater = 0; // reset timer
 				waterTR.enabled = true;
 			}
 		}
 
-		if (!filling && water < maxWater) {
+		/*if (!filling && water < maxWater) {
 			filling = true;
 			water += 1;
 			StartCoroutine(FillWater());
-		}
+		}*/
 	}
 
 	IEnumerator FillWater() { // TODO: redo with Invoke and anonymous function instead of coroutine
@@ -138,5 +141,10 @@ public class SubtropicsPlayer : MonoBehaviour {
 		// speed = Mathf.Lerp(slowSpeed,factor * slowSpeed, 0.25f); 
 		slow = false;
 		speed = factor * slowSpeed;
+	}
+
+	void UpdateSlider(Slider slider, float value)
+	{
+		slider.value = value;
 	}
 }
