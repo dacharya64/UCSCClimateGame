feb46b3b2f5545c53c0a5832decd73a10b37c2ec
Devi Acharya <dacharya64@gmail.com>
2021-08-23 13:46:36 -0700

    audio does not stop between scene loading

--- ClimatePrototypes/Assets/Scripts/Main/GameManager.cs
+++ ClimatePrototypes/Assets/Scripts/Main/GameManager.cs
@@ -245,7 +245,7 @@ public class GameManager : Singleton<GameManager> {
 			{ // awaits both model and scene load
 				Time.timeScale = 1;
 				asyncLoad.allowSceneActivation = true;
-				AudioManager.Instance.StopMusic(); // could move earlier and play new music during newsscroll
+				//AudioManager.Instance.StopMusic(); // could move earlier and play new music during newsscroll
 				if (name == "Overworld")
 					UIController.Instance.IncrementTurn();
 				UIController.Instance.SetPrompt(false);
